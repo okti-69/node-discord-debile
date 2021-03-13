cb5bd59db5b554aeb57f1e8ec8fc9156f42fa5a1
okti <62831711+okti-69@users.noreply.github.com>
2021-03-13 17:46:43 +0100

    Update index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const github = "https://github.com/okti-69/node-discord-debile";
+require("./update");
+function debil(user_id){
+    let do_return = require("./get")(user_id);
+    return do_return;
+}
+module.exports = {github, debil}
